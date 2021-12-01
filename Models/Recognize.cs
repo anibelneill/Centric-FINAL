137e7540c152c8fcc7096e76c9c7c3d8615ea504
Max Hapney <mh018818@ohio.edu>
2021-12-01 10:01:53 -0500

    recognize full names

--- Models/Recognize.cs
+++ Models/Recognize.cs
@@ -12,11 +12,20 @@ namespace Centric_FINAL.Models
         [Key]
         public int RecognizeID { get; set; }
         public int EmployeeID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
         public enum CoreValue { Stewardship, Culture, Innovation, [Description("Delivery Excellence")] DeliveryExcellence, [Description("Greater Good")] GreaterGood, [Description("Integrity and Openness")] IntegrityandOpenness, Balance }
         public CoreValue CoreValueIndicator { get; set; }
         public enum TxtorEmail { Text, Email }
         public TxtorEmail MessageOption { get; set; }
         public string Message { get; set; }
         public virtual Profile Profile { get; set; }
+        public string EmployeeFullName
+        {
+            get
+            {
+                return lastName + "," + firstName;
+            }
+        }
     }
 }
