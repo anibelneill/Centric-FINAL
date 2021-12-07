933b3f5070119a8ca31c5252a0f85161ee0c749e
Max Hapney <mh018818@ohio.edu>
2021-12-07 17:36:04 -0500

    a

---
+++ Models/TrackProgess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Centric_FINAL.Models
+{
+    public class TrackProgess
+    {
+        public int  TrackID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string BusinessUnitName { get; set; }
+        public string BusinessTitle { get; set; }
+
+    }
+}
