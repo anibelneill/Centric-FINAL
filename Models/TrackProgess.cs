e73998ec13e2171da03e575bd1095b7aa8efea94
Anibel Neill <an014317@ohio.edu>
2021-12-07 17:42:57 -0500

    j

--- Models/TrackProgess.cs
+++ Models/TrackProgess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace Centric_FINAL.Models
 {
     public class TrackProgess
     {
+        [Key]
         public int  TrackID { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
